eb800760032c5c371281aeb5d603af54c8bd4f75
Eugene-ak <amedioreugene@gmail.com>
2024-07-25 15:52:57 +0000

    Add a mobile nav menu

--- src/components/molecules/Headers.tsx
+++ src/components/molecules/Headers.tsx
@@ -5,12 +5,19 @@ import TrainingImage from "/icons/Training.png";
 import Hamburger from "/icons/menu.png";
 import { Link } from "react-router-dom";
 import headerStyles from "../../styles/component-styles/header.module.css";
+import { useState } from "react";
 
 export function PrimaryHeader() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const handleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
   return (
     <header className={headerStyles.primary_header}>
       <img src={Logo} alt="logo" />
-      <nav>
+      <nav className={headerStyles.desktop_nav}>
         <ul>
           <li>
             <img src={Briefcase} alt="briefcase" />
@@ -26,11 +33,39 @@ export function PrimaryHeader() {
           </li>
         </ul>
       </nav>
+      {isMobileNavOpen && (
+        <nav className={headerStyles.mobile_nav}>
+          <ul>
+            <li>
+              <img src={Briefcase} alt="briefcase" />
+              <span>Jobs</span>
+            </li>
+            <li>
+              <img src={UserStar} alt="user" />
+              <span>Mentorship</span>
+            </li>
+            <li>
+              <img src={TrainingImage} alt="training" />
+              <span>Trainings</span>
+            </li>
+          </ul>
+          <div>
+            <Link to={"/account-type"}>Log in</Link>
+            <Link to={"/account-type"}>Sign up</Link>
+          </div>
+        </nav>
+      )}
       <div>
         <Link to={"/account-type"}>Log in</Link>
         <Link to={"/account-type"}>Sign up</Link>
       </div>
-      <img src={Hamburger} className={headerStyles.menu_icon} alt="" />
+      <img
+        src={Hamburger}
+        className={headerStyles.menu_icon}
+        alt=""
+        title="Menu"
+        onClick={handleMobileNav}
+      />
     </header>
   );
 }
